ee64f85aecd2797ce71365fb8acb8abc2638c979
Joe <joesidious@gmail.com>
2023-04-30 09:36:25 +0100

    Bean (Corrections to the Bean)

--- src/main/java/com/example/springboothelloworld/SpringBootHelloworldApplication.java
+++ src/main/java/com/example/springboothelloworld/SpringBootHelloworldApplication.java
@@ -1,47 +1,41 @@
 package com.example.springboothelloworld;
 
-import jakarta.servlet.Servlet;
-import jakarta.servlet.ServletRegistration;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
-import com.example.springboothelloworld.servlet.HelloFilter;
-
-
+@SpringBootApplication
 public class SpringBootHelloworldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootHelloworldApplication.class, args);
     }
-
-    //@ServletComponentScan
-
+    /// Register Servlet.
     @Bean
-
     public ServletRegistrationBean getServletRegistrationBean() {
-        ServletRegistrationBean servletBean = new ServletRegistrationBean();
+        ServletRegistrationBean servletBean = new ServletRegistrationBean(new HelloServlet());
         servletBean.addUrlMappings("/helloServlet");
         return servletBean;
     }
 
-    //Register Filter
+    /// Register Filter.
     @Bean
     public FilterRegistrationBean getFilterRegistrationBean() {
-        FilterRegistrationBean filterBean = new FilterRegistrationBean();
-        //Add Filter path
-        filterBean.addUrlPatterns("./helloServlet");
+        FilterRegistrationBean filterBean = new FilterRegistrationBean(new HelloFilter());
+        // Add filter path
+        filterBean.addUrlPatterns("/helloServlet");
         return filterBean;
     }
 
     @Bean
-    public ServletListenerRegistrationBean<HelloListener> getServletRegistrationBean() {
-        ServletListenerRegistrationBean listenerBean = new ServletListenerRegistrationBean(new HelloListener());
+    public ServletListenerRegistrationBean<HelloListener> getServletListenerRegistrationBean() {
+        ServletListenerRegistrationBean listenerBean =
+                new ServletListenerRegistrationBean(new HelloListener());
         return listenerBean;
     }
-
 }
